7173008bc22479accc7627a2443fd1d5a22d67fc
Leticia-23 <sleticiasanchez23@gmail.com>
2023-10-06 09:53:17 +0200

    Initial commit

---
+++ src/main/java/com/hiberus/adoptionskafka/AdoptionsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.hiberus.adoptionskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionsKafkaApplication.class, args);
+	}
+
+}
