6d815cd5c973c78c08f965faede7c254668c71fb
Leticia-23 <sleticiasanchez23@gmail.com>
2023-10-06 11:19:25 +0200

    readme, docker-compose, firts-module wirh dependecies

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# adoptions-kafka
+Kafka project for Microservices Hiberus University
+
+# Usage guide
+1. ```git clone https://github.com/Leticia-23/adoptions-kafka.git```
+2. ``mvn clean install`` to install the project dependencies
+3. ``docker-compose up --build`` 
--- animals-sender/src/main/java/com/hiberus/adoptionskafka/AnimalsSenderApplication.java
+++ animals-sender/src/main/java/com/hiberus/adoptionskafka/AnimalsSenderApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdoptionsKafkaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AdoptionsKafkaApplication.class, args);
-	}
+public class AnimalsSenderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalsSenderApplication.class, args);
+    }
 
 }
