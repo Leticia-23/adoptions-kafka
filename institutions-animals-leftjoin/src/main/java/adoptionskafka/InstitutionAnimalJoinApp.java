12d54238dbd8e16cff6f349649c4d190820a5087
Leticia-23 <sleticiasanchez23@gmail.com>
2023-10-10 11:31:03 +0200

    joiner in progres

---
+++ institutions-animals-leftjoin/src/main/java/adoptionskafka/InstitutionAnimalJoinApp.java
@@ -0,0 +1,13 @@
+
+package adoptionskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutionAnimalJoinApp {
+    public static void main(String[] args) {
+        SpringApplication.run(InstitutionAnimalJoinApp.class, args);
+    }
+
+}
